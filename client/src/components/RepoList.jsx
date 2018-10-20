47aea70749b7429510e2e74f55d547bee271cb92
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-20 11:15:32 -0700

    Make RepoList display all RepoListItems

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -5,7 +5,7 @@ const RepoList = (props) => (
   <div>
     <h4> Repos With Most Commits </h4>
     <div>There are {props.repos.length} repos.</div>
-    {props.repos.length > 0 ? <RepoListItem repo={props.repos[0]} /> : ''}
+    {props.repos.map( repo => <RepoListItem key={repo._id}repo={repo} /> )}
   </div>
 )
 
